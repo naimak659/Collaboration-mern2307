da7116c23e53e4d94a70cb4c92c72c4407ee07d1
Sotabdi <mhsotabdi@gmail.com>
2024-09-01 12:30:55 +0600

    modified menu component

--- src/components/AgencSections/CommonComponents/Menu.jsx
+++ src/components/AgencSections/CommonComponents/Menu.jsx
@@ -4,13 +4,13 @@ const Menu = ({ menuItem }) => {
     //expect item array to create menu 
   return (
     <ul className="flex gap-x-[64px] w-[100%]">
-      {menuItem.map((item) => (
-        <li key={item}>
+      {menuItem.map((item,index) => (
+        <li key={index}>
           <a
-            href="#"
+            href={item.link}
             className="font-inter text-[18px] hover:text-[#6A4DF4] font-bold text-white transition ease-in-out delay-100"
           >
-            {item}
+            {item.label}
           </a>
         </li>
       ))}
--- src/components/AgencSections/Navbar/Navbar.jsx
+++ src/components/AgencSections/Navbar/Navbar.jsx
@@ -2,13 +2,19 @@ import logo from '../../../assets/navAssets/logo.png';
 import Menu from '../CommonComponents/Menu';
 import Button from '../CommonComponents/Button';
 const Navbar = () => {
+  const menuItems = [
+    { label: "Home", link: "/" },
+    { label: "About", link: "/about" },
+    { label: "Services", link: "/services" },
+    { label: "Contact", link: "/contact" },
+  ];
   return (
     <nav className='flex items-center justify-between py-[50px] absolute z-10 w-full'>
       <div className='w-[166.2px]'>
         <picture><img src={logo} alt={logo} /></picture>
       </div>
       <div>
-        <Menu menuItem={['Home','About','Service','Careers', 'Contact']}/>
+        <Menu menuItem={menuItems}/>
       </div>
       <div>
         <Button Content={'Contact'} todo={()=>{window.alert('todo')}}/>
